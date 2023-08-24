5720ef3d3e8fc84b55b03f872d38fe6bced51aad
Yang Ricardo Barcellos Miranda <yangricardo17@gmail.com>
2023-08-24 19:44:18 -0300

    feat: prisma as global module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from 'nestjs-prisma';
 
 @Module({
-  imports: [],
+  imports: [
+    PrismaModule.forRoot({
+      isGlobal: true,
+      prismaServiceOptions: {
+        prismaOptions: {
+          log: ['query', 'info', 'warn','error'],
+          errorFormat: 'pretty',
+        }
+      }
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
