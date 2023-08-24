92c8da545209b2b87b15a04e8380ed4f9db0d190
Yang Ricardo Barcellos Miranda <yangricardo17@gmail.com>
2023-08-24 19:52:18 -0300

    test: should create a new tweet

--- src/tweets/tweets.service.spec.ts
+++ src/tweets/tweets.service.spec.ts
@@ -1,18 +1,50 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TweetsService } from './tweets.service';
+import { mockDeep, DeepMockProxy } from 'jest-mock-extended';
+import { PrismaService } from 'nestjs-prisma';
 
 describe('TweetsService', () => {
   let service: TweetsService;
-
+  let prismaService: DeepMockProxy<PrismaService>;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TweetsService],
-    }).compile();
+      providers: [TweetsService,PrismaService],
+    })
+    .overrideProvider(PrismaService)
+    .useValue(mockDeep<PrismaService>())
+    .compile();
 
     service = module.get<TweetsService>(TweetsService);
+    prismaService = module.get(PrismaService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe(`createTweet`, () => {
+    it(`should create a new tweet`, async () => {
+      // Arrange
+      const mockedTweet = {
+        id: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        content: `Hello world, this is a tweet.`,
+        userId: 1234,
+      };
+      prismaService.tweet.create.mockResolvedValue(mockedTweet);
+
+      // Act
+      const createTweet = () =>
+        service.createTweet({
+          data: {
+            content: mockedTweet.content,
+            user: {
+              connect: {
+                id: mockedTweet.userId,
+              },
+            },
+          },
+        });
+
+      // Assert
+      await expect(createTweet()).resolves.toBe(mockedTweet);
+    });
   });
 });
