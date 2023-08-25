21f781b4e48971ce5e758f9750fc3cde28f321de
Yang Ricardo Barcellos Miranda <yangricardo17@gmail.com>
2023-08-25 15:26:22 -0300

    feat(bootstrap): log server data

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { Env } from './env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const { HOST, PORT, NODE_ENV, TZ } = Env.getInstance();
+  const logger = new Logger('bootstrap', {
+    timestamp: true,
+  });
+  await app.listen(PORT, HOST, async () => {
+    logger.log(
+      `Server is running in [${NODE_ENV}] mode on [http://${HOST}:${PORT}] with timezone [${TZ}]`,
+    );
+  });
 }
 bootstrap();
